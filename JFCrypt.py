9a32c59f5a246595c8385ee0af73102bc172b935
Jonnelafin <elias.eskelinen@pm.me>
2019-08-23 20:54:18 +0300

    Added comments

--- JFCrypt.py
+++ JFCrypt.py
@@ -6,8 +6,8 @@ print("JFCrypt by elias eskelinen aka 'Jonnelafin'")
 import numpy as np
 
 def encrypt(inp = [], password = 0):
-	key1 = 141650939
-	key2 = int(key1) * int(password)
+	key1 = 141650939 #Prime
+	key2 = int(key1) * int(password) #Generate a key from the password
 	out = []
 	iterator = 0
 	print("------Encrypt------")
@@ -24,14 +24,18 @@ def decrypt(lisd = [], password = 0):
 	out = []
 	iterator = 0
 	print("------Decrypt------")
+	comment = False
 	for i in lisd:
-		num = (int(i) / (key2))-(iterator)
-		try:
-			out.append(chr(int(num)))
-		except:
-			print("Problem with " + str(num))
-			out.append("[Incorrect encryption: " + str(num)+"]")
-		iterator = iterator + 1
+		if i[0] == "#":
+			print("Comment: " + i)
+		else:
+			num = (int(i) / (key2))-(iterator)
+			try:
+				out.append(chr(int(num)))
+			except:
+				print("Problem with " + str(num))
+				out.append("[Incorrect encryption: " + str(num)+"]")
+			iterator = iterator + 1
 	return(out)
 choice = input("(E)ncrypt or (D)ecrypt? ")
 if(choice == "E"):
@@ -52,6 +56,7 @@ if(choice == "E"):
 	lis = encrypt(inp, pswd)
 	with open('encrypted.txt', 'w+') as f:
 		f.truncate(0)
+		f.write("#Encrypted using JFCrypt\n")
 		for line in lis:
 			f.write(str(line)+"\n")
 	print("Done")
@@ -79,7 +84,6 @@ elif(choice == "D"):
 			f.write(s)
 	print("Done")
 print("Exit")
-
 
 
 
